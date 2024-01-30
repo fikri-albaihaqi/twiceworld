b98b5776886a0f47671d2d652bc25c4e8ca346cf
Muhamad Fikri Al-Baihaqi <fikrialbaihaqi@gmail.com>
2024-01-30 22:58:28 +0700

    Created a History component

---
+++ app/components/history.tsx
@@ -0,0 +1,45 @@
+import { dmSerif } from "../assets/fonts"
+
+export default function History({
+  id,
+  title,
+  date,
+  img,
+  description,
+} : {
+  id: number;
+  title: string;
+  date: string;
+  img: string;
+  description: string;
+}) {
+
+
+  return(
+    id % 2 !== 0 ? (
+      <div className="flex items-center h-[1080px]">
+      <div className="w-1/2 ml-16 pr-16">
+        <h1 className={`text-4xl font-bold ${dmSerif.className}`}>{title}</h1>
+        <h2 className={`font-bold ${dmSerif.className}`}>{date}</h2>
+        <p className="mt-6">
+          {description}
+        </p>  
+      </div>
+
+      <div className={`w-[60%] h-[1080px] relative right-0 bg-cover`} style={{backgroundImage: `url(${img})`}}></div>
+    </div>
+    ) : (
+      <div className="flex items-center h-[1080px]">
+        <div className={`w-[60%] h-[1080px] relative right-0 bg-cover`} style={{backgroundImage: `url(${img})`}}></div>
+
+        <div className="w-1/2 mr-16 pl-16">
+          <h1 className={`text-4xl font-bold ${dmSerif.className}`}>{title}</h1>
+          <h2 className={`font-bold ${dmSerif.className}`}>{date}</h2>
+          <p className="mt-6">
+            {description}
+          </p>  
+        </div>
+      </div>
+    )
+  )
+}
--- app/history/page.tsx
+++ app/history/page.tsx
@@ -1,52 +1,55 @@
-import { bungee, dmSerif } from "../assets/fonts"
-import Image from "next/image"
+import { bungee } from "../assets/fonts"
+import History from "../components/history"
 
-export default function History() {
+export default function Page() {
+  const histories = [
+    {
+      id: 1,
+      title: "PRE-DEBUT",
+      date: "2015",
+      img: "/sixteen.png",
+      description: "JYP's Brand new girl group 'TWICE' release debut album 'THE STORY BEGINS'TWICE will present vibrant energy with title track 'Like OOH-AHH'Average age is 18, They will take up with assertive attractiveness. - JYP Brand new girl group 'TWICE' release debut album 'THE STORY BEGINS' - Present the brand new genre \"\Color Pop\"\ which is unique color of TWICE - Once with superb music, Twice with best performance,Have great willingness to present the highest touching to the fans",
+    },
+    {
+      id: 2,
+      title: "THE DEBUT",
+      date: "2015",
+      img: "/ooh-ahh.png",
+      description: "JYP's Brand new girl group 'TWICE' release debut album 'THE STORY BEGINS'TWICE will present vibrant energy with title track 'Like OOH-AHH'Average age is 18, They will take up with assertive attractiveness. - JYP Brand new girl group 'TWICE' release debut album 'THE STORY BEGINS' - Present the brand new genre \"\Color Pop\"\ which is unique color of TWICE - Once with superb music, Twice with best performance,Have great willingness to present the highest touching to the fans",
+    },
+    {
+      id: 3,
+      title: "THE FIRST COMEBACK",
+      date: "2016",
+      img: "/sixteen.png",
+      description: "JYP's Brand new girl group 'TWICE' release debut album 'THE STORY BEGINS'TWICE will present vibrant energy with title track 'Like OOH-AHH'Average age is 18, They will take up with assertive attractiveness. - JYP Brand new girl group 'TWICE' release debut album 'THE STORY BEGINS' - Present the brand new genre \"\Color Pop\"\ which is unique color of TWICE - Once with superb music, Twice with best performance,Have great willingness to present the highest touching to the fans",
+    }
+  ]
+
   return(
     <main className="flex flex-col">
-      <section className="w-full flex flex-col justify-center items-center my-32">
+      <section className="w-full flex flex-col justify-center items-center mt-56 mb-32">
         <div className="flex justify-center mb-4">
-          <h1 className={`text-4xl text-primary-orange absolute -mr-3 -mt-1 text-center ${bungee.className}`}>
+          <h1 className={`text-6xl text-primary-orange absolute -mr-3 -mt-1 text-center ${bungee.className}`}>
             HISTORY
           </h1>
-          <h1 className={`text-4xl text-alternate-black text-center ${bungee.className}`}>
+          <h1 className={`text-6xl text-alternate-black text-center ${bungee.className}`}>
             HISTORY
           </h1>
         </div>
       </section>
 
       <section className="">
-        <div className="flex items-center h-screen">
-          <div className="w-1/2 ml-16 pr-16">
-            <h1 className={`text-4xl font-bold ${dmSerif.className}`}>PRE-DEBUT</h1>
-            <h2 className={`font-bold ${dmSerif.className}`}>2015</h2>
-            <p className="mt-6">
-              JYP's Brand new girl group 'TWICE' release debut album 'THE STORY BEGINS'TWICE will present vibrant energy with title track 'Like OOH-AHH'Average age is 18, They will take up with assertive attractiveness.
-
-              - JYP Brand new girl group 'TWICE' release debut album 'THE STORY BEGINS'
-              - Present the brand new genre "Color Pop" which is unique color of TWICE
-              - Once with superb music, Twice with best performance,Have great willingness to present the highest touching to the fans  
-            </p>  
-          </div>
-
-          <div className="w-1/2 h-screen relative right-0 bg-[url('/sixteen.png')] bg-cover"></div>
-        </div>
-
-        <div className="flex items-center h-screen">
-          <div className="w-1/2 h-screen relative right-0 bg-[url('/ooh-ahh.png')] bg-cover"></div>
-
-          <div className="w-1/2 mr-16 pl-16">
-            <h1 className={`text-4xl font-bold ${dmSerif.className}`}>THE DEBUT</h1>
-            <h2 className={`font-bold ${dmSerif.className}`}>2015</h2>
-            <p className="mt-6">
-              JYP's Brand new girl group 'TWICE' release debut album 'THE STORY BEGINS'TWICE will present vibrant energy with title track 'Like OOH-AHH'Average age is 18, They will take up with assertive attractiveness.
-
-              - JYP Brand new girl group 'TWICE' release debut album 'THE STORY BEGINS'
-              - Present the brand new genre "Color Pop" which is unique color of TWICE
-              - Once with superb music, Twice with best performance,Have great willingness to present the highest touching to the fans  
-            </p>  
-          </div>
-        </div>
+        {histories?.map((history) => (
+          <History 
+            key={history.id} 
+            id={history.id} 
+            title={history.title} 
+            date={history.date} 
+            img={history.img}
+            description={history.description} 
+          />
+        ))}
       </section>
     </main>
   )
