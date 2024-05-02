541393cc819e898c699478f5dcf34a06827f1d33
Muhamad Fikri Al-Baihaqi <fikrialbaihaqi@gmail.com>
2024-05-02 20:57:29 +0700

    fix: add header change image in history page

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -8,37 +8,30 @@ export default function Page() {
       id: 1,
       title: "PRE-DEBUT",
       date: "2015",
-      img: "/sixteen.png",
+      img: "/sixteen-1.png",
       description: "JYP's Brand new girl group 'TWICE' release debut album 'THE STORY BEGINS'TWICE will present vibrant energy with title track 'Like OOH-AHH'Average age is 18, They will take up with assertive attractiveness. - JYP Brand new girl group 'TWICE' release debut album 'THE STORY BEGINS' - Present the brand new genre \"\Color Pop\"\ which is unique color of TWICE - Once with superb music, Twice with best performance,Have great willingness to present the highest touching to the fans",
     },
     {
       id: 2,
       title: "THE DEBUT",
       date: "2015",
-      img: "/ooh-ahh.png",
+      img: "/the-story-begins-1.png",
       description: "JYP's Brand new girl group 'TWICE' release debut album 'THE STORY BEGINS'TWICE will present vibrant energy with title track 'Like OOH-AHH'Average age is 18, They will take up with assertive attractiveness. - JYP Brand new girl group 'TWICE' release debut album 'THE STORY BEGINS' - Present the brand new genre \"\Color Pop\"\ which is unique color of TWICE - Once with superb music, Twice with best performance,Have great willingness to present the highest touching to the fans",
     },
     {
       id: 3,
       title: "THE FIRST COMEBACK",
       date: "2016",
-      img: "/sixteen.png",
+      img: "/sixteen-1.png",
       description: "JYP's Brand new girl group 'TWICE' release debut album 'THE STORY BEGINS'TWICE will present vibrant energy with title track 'Like OOH-AHH'Average age is 18, They will take up with assertive attractiveness. - JYP Brand new girl group 'TWICE' release debut album 'THE STORY BEGINS' - Present the brand new genre \"\Color Pop\"\ which is unique color of TWICE - Once with superb music, Twice with best performance,Have great willingness to present the highest touching to the fans",
     }
   ]
 
   return(
     <main className="flex flex-col">
-      <section className="w-full flex flex-col justify-center items-center mt-56 mb-32">
-        <div className="flex justify-center mb-4">
-          <h1 className={`text-6xl text-primary-orange absolute -mr-3 -mt-1 text-center ${bungee.className}`}>
-            HISTORY
-          </h1>
-          <h1 className={`text-6xl text-alternate-black text-center ${bungee.className}`}>
-            HISTORY
-          </h1>
-        </div>
-      </section>
+      <header className="relative -z-10 h-[560px] md:h-[440px] lg:h-[640px] xl:h-[900px]">
+        <Image src="/history-header.png" alt="Twice Ready To Be Concert in Las Vegas" fill={true} />
+      </header>
 
       <section className="">
         {histories?.map((history) => (
