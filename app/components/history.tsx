3700c5201424a5b3d44571ccb67aba75065c6e5f
Muhamad Fikri Al-Baihaqi <fikrialbaihaqi@gmail.com>
2024-05-04 21:13:07 +0700

    fix: adjust the history layout for mobile view

--- app/components/history.tsx
+++ app/components/history.tsx
@@ -18,8 +18,8 @@ export default function History({
 
   return(
     id % 2 !== 0 ? (
-      <div className="flex justify-between items-center my-16">
-        <div className="w-1/2">
+      <div className="flex flex-col-reverse md:flex-row justify-between items-center my-16">
+        <div className="w-2/3 lg:w-1/2">
           <h1 className={`text-4xl font-bold ${poppins.className}`}>{title}</h1>
           <h2 className={`font-bold ${poppins.className}`}>{date}</h2>
           <p className="mt-6">
@@ -27,13 +27,13 @@ export default function History({
           </p>  
         </div>
       
-        <Image src={img} alt={description} width={500} height={0} className="right-0" />
+        <Image src={img} alt={description} width={500} height={0} className="right-0 ml-4" />
       </div>
     ) : (
-      <div className="flex justify-between items-center my-16">
-        <Image src={img} alt={description} width={500} height={0} className="right-0" />
+      <div className="flex flex-col md:flex-row justify-between items-center my-16">
+        <Image src={img} alt={description} width={500} height={0} className="left-0 mr-4" />
 
-        <div className="w-1/2 mr-16 pl-16">
+        <div className="w-2/3 lg:w-1/2">
           <h1 className={`text-4xl font-bold ${poppins.className}`}>{title}</h1>
           <h2 className={`font-bold ${poppins.className}`}>{date}</h2>
           <p className="mt-6">
