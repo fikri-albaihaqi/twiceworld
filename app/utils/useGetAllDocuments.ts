960de8440393155941170a7782c016df65f24648
Muhamad Fikri Al-Baihaqi <fikrialbaihaqi@gmail.com>
2024-06-19 21:48:09 +0700

    feat: add new parameters for order and the direction

--- app/utils/useGetAllDocuments.ts
+++ app/utils/useGetAllDocuments.ts
@@ -1,12 +1,22 @@
-import { collection, getDocs } from "@firebase/firestore";
+import { collection, getDocs, orderBy, query } from "@firebase/firestore";
 import db from "./firestore";
 
 export const useGetAllDocuments = () => {
 
-  const getDoc = async (path: string) => {
+  const getDoc = async (path: string, order?: string, direction?: string) => {
 
     const collectionRef = collection(db, path)
-    const querySnapshot = await getDocs(collectionRef)
+    let q
+    if (order) {
+      if (direction === "desc") {
+        q = query(collectionRef, orderBy(order, "desc"))
+      } else {
+        q = query(collectionRef, orderBy(order, "asc"))
+      }
+    } else {
+      q = query(collectionRef)
+    }
+    const querySnapshot = await getDocs(q)
     const data = querySnapshot.docs.map((doc) => ({
       ...doc.data(),
       id: doc.id,
