437552c38392ee41c4f9a610e86bf5fe1d02ac7e
Muhamad Fikri Al-Baihaqi <fikrialbaihaqi@gmail.com>
2024-06-19 22:01:09 +0700

    feat: add twice logo

--- app/components/adminMenu.tsx
+++ app/components/adminMenu.tsx
@@ -3,6 +3,8 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { auth } from "../utils/firebaseConfig"
 import { faArrowRightFromBracket } from "@fortawesome/free-solid-svg-icons"
+import Link from "next/link"
+import Image from "next/image"
 
 const AdminMenu = () => {
   const handleLogout = () => {
@@ -10,11 +12,13 @@ const AdminMenu = () => {
   }
 
   return (
-    <nav className="w-screen flex justify-between px-16 py-4 bg-alternate-black text-white">
-      <h1>Welcome to Twiceland Dashboard!</h1>
+    <nav className="w-screen flex justify-between px-8 py-4 bg-alternate-black text-white">
+      <Link href="/admin" className="w-[48px] md:w-[64px]" >
+        <Image src="/twice-logo.svg" alt="Twice logo" width={64} height={0} />
+      </Link>
       <button onClick={handleLogout} className="">
         Logout
-        <FontAwesomeIcon icon={faArrowRightFromBracket} size="lg" style={{color: "#ffff"}} className="ml-2" />
+        <FontAwesomeIcon icon={faArrowRightFromBracket} size="lg" style={{ color: "#ffff" }} className="ml-2" />
       </button>
     </nav>
   )
