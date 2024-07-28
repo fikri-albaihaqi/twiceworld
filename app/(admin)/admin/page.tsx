d6c88cbf6d64e475888411c7b7e28a1fb2c55050
Muhamad Fikri Al-Baihaqi <fikrialbaihaqi@gmail.com>
2024-07-28 19:11:26 +0700

    feat: add pagination in admin discography page

--- app/(admin)/admin/page.tsx
+++ app/(admin)/admin/page.tsx
@@ -3,23 +3,43 @@
 import Discography from "@/app/components/discography"
 import db from "@/app/utils/firestore"
 import { useGetAllDocuments } from "@/app/utils/useGetAllDocuments"
-import { collection, orderBy, query } from "@firebase/firestore"
+import { collection, endBefore, limit, orderBy, query, startAfter } from "@firebase/firestore"
 import { faPlus } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import Link from "next/link"
 import { useEffect, useState } from "react"
 
 const Page = () => {
+  const [page, setPage] = useState<number>(1)
+  const pageSize = 8;
 
   const [discography, setDiscography] = useState<any>([])
   const { getAllDocuments } = useGetAllDocuments()
 
   const collectionRef = collection(db, "discography")
-  const dbQuery = query(collectionRef, orderBy("releaseDate", "desc"))
+  const dbQuery = query(collectionRef, orderBy("releaseDate", "desc"), limit(pageSize))
 
   useEffect(() => {
     getAllDocuments(dbQuery).then(data => setDiscography(data))
   }, [])
+
+  const nextPage = ({ item }: any) => {
+    if (discography.length === 0) {
+      alert("Thats all the song for now!")
+    } else {
+      const collectionRef = collection(db, "discography")
+      const dbQuery = query(collectionRef, orderBy("releaseDate", "desc"), limit(pageSize), startAfter(item.releaseDate))
+      getAllDocuments(dbQuery).then(data => setDiscography(data))
+      setPage(page + 1)
+    }
+  }
+
+  const previousPage = ({ item }: any) => {
+    const collectionRef = collection(db, "discography")
+    const dbQuery = query(collectionRef, orderBy("releaseDate", "desc"), limit(pageSize), endBefore(item.releaseDate))
+    getAllDocuments(dbQuery).then(data => setDiscography(data))
+    setPage(page - 1)
+  }
 
   return (
     <main>
@@ -32,6 +52,15 @@ const Page = () => {
         </Link>
         <div className="grid grid-cols-4">
           {discography.map((item: any) => <Discography album={item} admin={true} key={item.id} />)}
+        </div>
+        <div className="flex justify-center mt-8">
+          {
+            page === 1 ? '' : <button onClick={() => previousPage({ item: discography[0] })} className="mx-2">Previous</button>
+          }
+
+          {
+            discography.length < 8 ? '' : <button onClick={() => nextPage({ item: discography[discography.length - 1] })} className="mx-2">Next</button>
+          }
         </div>
       </div>
     </main>
