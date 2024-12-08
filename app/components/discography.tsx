cd644a2f3511d50a8245a9c98239b92bab3daaa7
Muhamad Fikri Al-Baihaqi <fikrialbaihaqi@gmail.com>
2024-12-08 21:16:58 +0700

    fix: text visibility in discography component

--- app/components/discography.tsx
+++ app/components/discography.tsx
@@ -15,13 +15,13 @@ const Discography = ({ album, admin }: { album: DiscographyType; admin: boolean 
       <div className="w-full h-full flex flex-col justify-center items-center bg-transparent">
         <div className="absolute z-10 text-center">
           <h1
-            className="text-center text-2xl font-bold opacity-0 group-hover:opacity-100 transition-all duration-500 
+            className="text-gray-950 text-center text-2xl font-bold opacity-0 group-hover:opacity-100 transition-all duration-500 
                   ease-in-out"
           >
             {album.name}
           </h1>
           <p
-            className="text-base font-bold opacity-0 group-hover:opacity-100 transition-all duration-500 
+            className="text-gray-950 text-base font-bold opacity-0 group-hover:opacity-100 transition-all duration-500 
                   ease-in-out"
           >
             {album.releaseDate}
