6bb11998d28891d2fae10aa4c88b68bdae0c519d
Muhamad Fikri Al-Baihaqi <fikrialbaihaqi@gmail.com>
2024-12-18 18:57:27 +0700

    feat: add antd upload component
    add antd upload component for uploading album cover image

--- app/(admin)/admin/discography/add/page.tsx
+++ app/(admin)/admin/discography/add/page.tsx
@@ -3,15 +3,57 @@
 import { useState } from 'react'
 import { useAddDocument } from '@/app/lib/utils/useAddDocument'
 import { useRouter } from 'next/navigation'
-import { DatePicker, DatePickerProps, Form, Input, Select } from 'antd'
+import {
+  DatePicker,
+  DatePickerProps,
+  Form,
+  Image,
+  Input,
+  Select,
+  Upload,
+  UploadFile,
+  UploadProps,
+} from 'antd'
 import { DiscographyType } from '@/app/lib/types/firebase'
+import { FileType } from '@/app/lib/types/field'
+import { PlusOutlined } from '@ant-design/icons'
 
 const { Option } = Select
+
+const getBase64 = (file: FileType): Promise<string> =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onload = () => resolve(reader.result as string)
+    reader.onerror = (error) => reject(error)
+  })
 
 const Page = () => {
   const router = useRouter()
   const [form] = Form.useForm()
   const [scheduleDate, setScheduleDate] = useState<string | string[]>('')
+  const [previewOpen, setPreviewOpen] = useState(false)
+  const [previewImage, setPreviewImage] = useState('')
+  const [fileList, setFileList] = useState<UploadFile[]>([])
+
+  const handlePreview = async (file: UploadFile) => {
+    if (!file.url && !file.preview) {
+      file.preview = await getBase64(file.originFileObj as FileType)
+    }
+
+    setPreviewImage(file.url || (file.preview as string))
+    setPreviewOpen(true)
+  }
+
+  const handleChange: UploadProps['onChange'] = ({ fileList: newFileList }) =>
+    setFileList(newFileList)
+
+  const uploadButton = (
+    <button style={{ border: 0, background: 'none' }} type="button">
+      <PlusOutlined />
+      <div style={{ marginTop: 8 }}>Upload</div>
+    </button>
+  )
 
   const addDocument = useAddDocument()
 
@@ -32,9 +74,11 @@ const Page = () => {
   }
 
   return (
-    <main className="mx-16 mb-16">
-      <h1 className="text-2xl font-bold mt-8">Add New Album</h1>
-      <div className="w-1/2">
+    <main className="w-full flex flex-col items-center mb-16">
+      <h1 className="w-full text-center text-2xl font-bold my-8">
+        Add New Album
+      </h1>
+      <div className="w-1/2 flex justify-center">
         <Form
           layout="vertical"
           form={form}
@@ -71,24 +115,24 @@ const Page = () => {
             </Form.Item>
           </div>
 
-          <div className="flex">
-            <Form.Item<DiscographyType>
-              name="totalTrack"
-              label="Total Track"
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input the album total track!',
-                },
-              ]}
-              className="w-1/2 mr-2"
-            >
-              <Input
-                className="h-12 rounded-lg"
-                placeholder="Album Total Track"
-              />
-            </Form.Item>
+          <Form.Item<DiscographyType>
+            name="totalTrack"
+            label="Total Track"
+            rules={[
+              {
+                required: true,
+                message: 'Please input the album total track!',
+              },
+            ]}
+            className="w-1/2 mr-2"
+          >
+            <Input
+              className="h-12 rounded-lg"
+              placeholder="Album Total Track"
+            />
+          </Form.Item>
 
+          <div className="flex">
             <Form.Item<DiscographyType>
               name="image"
               label="Album Cover"
@@ -97,23 +141,44 @@ const Page = () => {
               ]}
               className="w-1/2"
             >
-              <Input className="h-12 rounded-lg" placeholder="Album Cover" />
+              <Upload
+                action="https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload"
+                listType="picture-card"
+                fileList={fileList}
+                onPreview={handlePreview}
+                onChange={handleChange}
+              >
+                {fileList.length >= 8 ? null : uploadButton}
+              </Upload>
+              {previewImage && (
+                <Image
+                  wrapperStyle={{ display: 'none' }}
+                  preview={{
+                    visible: previewOpen,
+                    onVisibleChange: (visible) => setPreviewOpen(visible),
+                    afterOpenChange: (visible) =>
+                      !visible && setPreviewImage(''),
+                  }}
+                  src={previewImage}
+                  alt="Album cover preview image"
+                />
+              )}
+            </Form.Item>
+
+            <Form.Item<DiscographyType>
+              name="releaseDate"
+              label="Release Date"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input the album release date!',
+                },
+              ]}
+              className="w-1/2 mr-2"
+            >
+              <DatePicker onChange={onChange} />
             </Form.Item>
           </div>
-
-          <Form.Item<DiscographyType>
-            name="releaseDate"
-            label="Release Date"
-            rules={[
-              {
-                required: true,
-                message: 'Please input the album release date!',
-              },
-            ]}
-            className="w-1/2 mr-2"
-          >
-            <DatePicker onChange={onChange} />
-          </Form.Item>
 
           <div className="flex">
             <Form.Item<DiscographyType>
--- app/lib/types/field.ts
+++ app/lib/types/field.ts
@@ -1,4 +1,8 @@
+import { GetProp, UploadProps } from "antd";
+
 export type AuthFieldType = {
   email: string
   password: string
 }
+
+export type FileType = Parameters<GetProp<UploadProps, 'beforeUpload'>>[0];
