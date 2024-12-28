d18dbf717898256750d7ca8c1ae31927699b0c2f
Muhamad Fikri Al-Baihaqi <fikrialbaihaqi@gmail.com>
2024-12-28 20:18:10 +0700

    feat: use antd form for the edit album form

--- app/(admin)/admin/discography/edit/[id]/page.tsx
+++ app/(admin)/admin/discography/edit/[id]/page.tsx
@@ -1,153 +1,296 @@
-"use client"
+'use client'
 
-import { useEffect, useState } from "react"
-import { useRouter } from "next/navigation"
-import { useUpdateDocument } from "@/app/lib/utils/useUpdateDocument"
-import { useGetDocument } from "@/app/lib/utils/useGetDocument"
-import { DocumentData } from "@firebase/firestore"
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { useUpdateDocument } from '@/app/lib/utils/useUpdateDocument'
+import { useGetDocument } from '@/app/lib/utils/useGetDocument'
+import {
+  DatePicker,
+  DatePickerProps,
+  Form,
+  Input,
+  Select,
+  Upload,
+  UploadFile,
+  UploadProps,
+} from 'antd'
+import { getStorage, ref, uploadBytes } from 'firebase/storage'
+import { DiscographyType } from '@/app/lib/types/firebase'
+import { PlusOutlined } from '@ant-design/icons'
+import dayjs from 'dayjs'
 
-const Page = ({
-  params,
-}: {
-  params: { id: string }
-}) => {
+const { Option } = Select
+
+const Page = ({ params }: { params: { id: string } }) => {
   const router = useRouter()
   const { getDocument } = useGetDocument()
+  const [form] = Form.useForm()
+  const [releaseDate, setReleaseDate] = useState<string | string[]>('')
+  const [fileList, setFileList] = useState<UploadFile[]>([])
+  const [imageFile, setImageFile] = useState<File | null>()
+  const storage = getStorage()
 
-  const [discography, setDiscography] = useState<DocumentData | undefined>({
-    name: "",
-    alternateName: "",
-    totalTrack: "",
-    image: "",
-    releaseDate: "",
-    type: "",
-    language: "",
-    video: "",
-    description: "",
-  })
+  const propsImage: UploadProps = {
+    onRemove: () => {
+      if (imageFile) {
+        setImageFile(null)
+      }
+    },
+    beforeUpload: (file) => {
+      setImageFile(file)
 
-  const onChange = (e: { target: { name: any; value: any } }) => {
-    setDiscography({ ...discography, [e.target.name]: e.target.value })
+      return false
+    },
+    accept: 'image/*',
+    maxCount: 1,
   }
 
   useEffect(() => {
-    getDocument("discography", params.id).then(data => setDiscography(data))
+    getDocument('discography', params.id).then((data) => {
+      form.setFieldsValue({ ...data, releaseDate: dayjs(data?.releaseDate) })
+      setReleaseDate(data?.releaseDate)
+      setFileList([
+        {
+          uid: '1',
+          name: data?.image,
+          status: 'done',
+          url: data?.image,
+        },
+      ])
+    })
   }, [])
+
+  const handleChange: UploadProps['onChange'] = ({ fileList: newFileList }) =>
+    setFileList(newFileList)
+
+  const uploadButton = (
+    <button style={{ border: 0, background: 'none' }} type="button">
+      <PlusOutlined />
+      <div style={{ marginTop: 8 }}>Upload</div>
+    </button>
+  )
+
+  const onChange: DatePickerProps['onChange'] = (_date, dateString) => {
+    setReleaseDate(dateString)
+  }
 
   const updateDocument = useUpdateDocument()
 
-  const handleInput = () => {
+  const onFinish = () => {
     try {
-      updateDocument("discography", params.id, discography)
-      router.push("/admin")
-    } catch (error) {
-      alert(error)
+      if (imageFile) {
+        const storageRef = ref(storage, 'album/' + imageFile.name)
+        uploadBytes(storageRef, imageFile).then((snapshot) => {
+          updateDocument('discography', params.id, {
+            ...form.getFieldsValue(),
+            image:
+              'https://firebasestorage.googleapis.com/v0/b/' +
+              snapshot.metadata.bucket +
+              '/o/album%2F' +
+              snapshot.metadata.name +
+              '?alt=media',
+            releaseDate: releaseDate,
+          }).then(() => router.push('/admin'))
+        })
+      } else {
+        updateDocument('discography', params.id, {
+          ...form.getFieldsValue(),
+          releaseDate: releaseDate,
+        }).then(() => router.push('/admin'))
+      }
+    } catch (error: any) {
+      alert('Edit Album Failed ' + error.message)
     }
   }
 
   return (
-    <main className="mx-16 mb-16">
-      <h1 className="text-2xl font-bold mt-8">Edit Discography</h1>
-      <form action="" className="w-1/2 mt-4">
-        <div className="flex justify-between my-2">
-          <label>Name</label>
-          <input
-            type="text"
-            name="name"
-            value={discography?.name}
-            onChange={onChange}
-            placeholder="ex: Feel Special"
-            className="px-2"
-          />
-        </div>
-        <div className="flex justify-between my-2">
-          <label>Alternate Name</label>
-          <input
-            type="text"
-            name="alternateName"
-            value={discography?.alternateName}
-            onChange={onChange}
-            placeholder="ex: The 5th Mini Album"
-            className="px-2"
-          />
-        </div>
-        <div className="flex justify-between my-2">
-          <label>Total Track</label>
-          <input
-            type="number"
+    <main className="w-full flex flex-col items-center mb-16">
+      <h1 className="w-full text-center text-2xl font-bold my-8">
+        Edit Discography
+      </h1>
+      <div className="w-1/2 flex justify-center">
+        <Form
+          layout="vertical"
+          form={form}
+          onFinish={onFinish}
+          className="w-[75%]"
+        >
+          <div className="flex">
+            <Form.Item<DiscographyType>
+              name="name"
+              label="Album Name"
+              rules={[
+                { required: true, message: 'Please input the album name!' },
+              ]}
+              className="w-1/2 mr-2"
+            >
+              <Input className="h-12 rounded-lg" placeholder="Album Name" />
+            </Form.Item>
+
+            <Form.Item<DiscographyType>
+              name="alternateName"
+              label="Alternate Name"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input the album alternate name!',
+                },
+              ]}
+              className="w-1/2"
+            >
+              <Input
+                className="h-12 rounded-lg"
+                placeholder="Album Alternate Name"
+              />
+            </Form.Item>
+          </div>
+
+          <Form.Item<DiscographyType>
             name="totalTrack"
-            value={discography?.totalTrack}
-            onChange={onChange}
-            placeholder="Total track"
-            className="px-2"
-          />
-        </div>
-        <div className="flex justify-between my-2">
-          <label>Image</label>
-          <input
-            type="text"
-            name="image"
-            value={discography?.image}
-            onChange={onChange}
-            placeholder="Put the link to the image"
-            className="px-2"
-          />
-        </div>
-        <div className="flex justify-between my-2">
-          <label>Release Date</label>
-          <input
-            type="text"
-            name="releaseDate"
-            value={discography?.releaseDate}
-            onChange={onChange}
-            placeholder="Release date (YYYY-MM-DD)"
-            className="px-2"
-          />
-        </div>
-        <div className="flex justify-between my-2">
-          <label>Type</label>
-          <select name="type" id="type" value={discography?.type} onChange={onChange}>
-            <option value="">Choose</option>
-            <option value="Full Album">Full Album</option>
-            <option value="Mini Album">Mini Album</option>
-            <option value="Special Album">Special Album</option>
-            <option value="Single">Single</option>
-          </select>
-        </div>
-        <div className="flex justify-between my-2">
-          <label>Language</label>
-          <select name="language" id="language" value={discography?.language} onChange={onChange}>
-            <option value="">Choose</option>
-            <option value="Korean">Korean</option>
-            <option value="Japanese">Japanese</option>
-            <option value="English">English</option>
-          </select>
-        </div>
-        <div className="flex justify-between my-2">
-          <label>Video</label>
-          <input
-            type="text"
+            label="Total Track"
+            rules={[
+              {
+                required: true,
+                message: 'Please input the album total track!',
+              },
+            ]}
+            className="w-1/2 mr-2"
+          >
+            <Input
+              className="h-12 rounded-lg"
+              placeholder="Album Total Track"
+            />
+          </Form.Item>
+
+          <div className="flex">
+            <Form.Item<DiscographyType>
+              name="image"
+              label="Album Cover"
+              className="w-1/2"
+            >
+              <Upload
+                listType="picture-card"
+                fileList={fileList}
+                onChange={handleChange}
+                {...propsImage}
+              >
+                {fileList.length >= 1 ? null : uploadButton}
+              </Upload>
+            </Form.Item>
+
+            <Form.Item<DiscographyType>
+              name="releaseDate"
+              label="Release Date"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input the album release date!',
+                },
+              ]}
+              className="w-1/2 mr-2"
+            >
+              <DatePicker onChange={onChange} />
+            </Form.Item>
+          </div>
+
+          <div className="flex">
+            <Form.Item<DiscographyType>
+              name="type"
+              label="Type"
+              rules={[
+                { required: true, message: 'Please input the album type' },
+              ]}
+              className="w-1/2 mr-2"
+            >
+              <Select
+                style={{ width: '100%' }}
+                className="h-12 rounded-lg"
+                placeholder="Choose Release Type"
+              >
+                <Option key="Full Album" value="Full Album">
+                  Full Album
+                </Option>
+                <Option key="Mini Album" value="Mini Album">
+                  Mini Album
+                </Option>
+                <Option key="Special Album" value="Special Album">
+                  Special Album
+                </Option>
+                <Option key="Single" value="Single">
+                  Single
+                </Option>
+              </Select>
+            </Form.Item>
+
+            <Form.Item<DiscographyType>
+              name="language"
+              label="Language"
+              rules={[
+                { required: true, message: 'Please input the album language!' },
+              ]}
+              className="w-1/2"
+            >
+              <Select
+                style={{ width: '100%' }}
+                className="h-12 rounded-lg"
+                placeholder="Choose Release Type"
+              >
+                <Option key="Korean" value="Korean">
+                  Korean
+                </Option>
+                <Option key="Japanese" value="Japanese">
+                  Japanese
+                </Option>
+                <Option key="English" value="English">
+                  English
+                </Option>
+              </Select>
+            </Form.Item>
+          </div>
+
+          <Form.Item<DiscographyType>
             name="video"
-            value={discography?.video}
-            onChange={onChange}
-            placeholder="Title track YouTube link"
-            className="px-2"
-          />
-        </div>
-        <div className="flex justify-between my-2">
-          <label>Description</label>
-          <textarea
+            label="Music Video Link"
+            rules={[
+              {
+                required: true,
+                message: 'Please input the album music video link!',
+              },
+            ]}
+            className="w-1/2"
+          >
+            <Input
+              className="h-12 rounded-lg"
+              placeholder="Album Music Video Link"
+            />
+          </Form.Item>
+
+          <Form.Item<DiscographyType>
             name="description"
-            value={discography?.description}
-            onChange={onChange}
-            placeholder="Discography description"
-            className="px-2 h-[150px]"
-          />
-        </div>
-        <button onClick={handleInput} type="button" className="bg-primary-pink text-white w-min p-2 mt-8 rounded">
-          Submit
-        </button>
-      </form>
+            label="Description"
+            rules={[
+              {
+                required: true,
+                message: 'Please input the album description!',
+              },
+            ]}
+          >
+            <Input.TextArea
+              className="h-12 rounded-lg"
+              placeholder="Album Description"
+              rows={8}
+            />
+          </Form.Item>
+          <Form.Item>
+            <button
+              className="w-full px-4 py-2 rounded-md bg-primary-pink text-white text-lg"
+              type="submit"
+            >
+              Edit Album
+            </button>
+          </Form.Item>
+        </Form>
+      </div>
     </main>
   )
 }
