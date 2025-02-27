2db0fb1c3c0bc0aa40a3fe6adb7a84b4184d2e36
Muhamad Fikri Al-Baihaqi <fikrialbaihaqi@gmail.com>
2025-02-27 14:53:39 +0700

    feat: add lenis smooth scroll

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -3,13 +3,14 @@ import '../globals.css'
 import Menu from '../components/menu'
 import { poppins } from '../assets/fonts'
 import Footer from '../components/footer'
+import { ReactLenis } from '../lib/utils/lenis'
 
 export const metadata: Metadata = {
   title: 'Twiceworld',
   description: 'Everything about the best K-Pop Girl Group TWICE!',
   icons: {
-    icon: "/favicon.png",
-  }
+    icon: '/favicon.png',
+  },
 }
 
 export default function RootLayout({
@@ -19,13 +20,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${poppins.className}`}>
-      <body
-        className="bg-[url('/bg-1.png')] bg-cover bg-fixed overflow-x-hidden text-alternate-black"
-      >
-        <Menu />
-        {children}
-        <Footer />
-      </body>
+      <ReactLenis root>
+        <body className="bg-[url('/bg-1.png')] bg-cover bg-fixed overflow-x-hidden text-alternate-black">
+          <Menu />
+          {children}
+          <Footer />
+        </body>
+      </ReactLenis>
     </html>
   )
 }
